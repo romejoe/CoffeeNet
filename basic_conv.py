fa69492b7149af1cce86d0ce0d48b3864d6f14d1
Joseph Stanton <jsromejoe@gmail.com>
2018-06-04 00:03:31 -0400

    Toyed a bit

--- basic_conv.py
+++ basic_conv.py
@@ -5,7 +5,9 @@ from __future__ import print_function
 import argparse
 
 import torch.nn as nn
-
+import torch.nn.functional as F
+#import matplotlib.pyplot as plt
+from torchvision.transforms import Resize
 from CoffeeDataset import CoffeeDataset
 
 parser = argparse.ArgumentParser()
@@ -15,13 +17,27 @@ parser.add_argument("--data_root", type=str, default='data')
 
 config = parser.parse_args()
 
-
-ds = CoffeeDataset('./data/labels.csv', './data/')
+ds = CoffeeDataset('./data/labels.csv', './data/', transform=Resize(224))
 
 tmp = ds[0]
 
-tmp[1].show()
+#tmp[1].show()
+
 
 class BasicConv(nn.Module):
     def __init__(self):
         super(BasicConv, self).__init__()
+        self.conv1 = nn.Conv2d(3 * 224, 64, kernel_size=5, stride=2)
+        self.fc = nn.Linear(64, 1)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.fc(x)
+
+        return x
+
+
+net = BasicConv()
+print(net)
+
+print(net.forward(tmp[0]))
